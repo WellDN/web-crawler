d47852c63ebbd3f3cf3c27b9803674d53350918a
WellDN <Wellingtontp0@gmail.com>
2024-02-19 13:35:00 -0300

    crawl by tag src

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 
 TODO:
 
-1: Start with seed. A web crawler starts with a list of URLs to visit. Make the seed function get information of an specific url.
-
-2: From the seed function make a function that take the url specific content and display in another place (your web localhost).
-
-3: Thus will form the crawler, and for the crawler function (using those 2 function above) we make a function that downloads the web page (with limited number of web pages
-within a given time). that what make the crawler.
+- set User-Agent Header to mimic a real browser
+- Map a web content to another website
+- Implement Robots Exclusion Protocol (read robots.txt)
 
 # Crawling Policy
 
@@ -16,5 +13,3 @@ within a given time). that what make the crawler.
 - a re-visit policy which states when to check for changes to the pages,
 - a politeness policy that states how to avoid overloading Web sites.
 - a parallelization policy that states how to coordinate distributed web crawlers.
-
-
--- cmd/web_crawler/main.go
+++ cmd/web_crawler/main.go
@@ -1,106 +1,94 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"golang.org/x/net/html"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+    "golang.org/x/net/html"
 )
 
 func main() {
-	// URL to start crawling
-	url := "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
+    // URL to start crawling
+    // TODO: Implement in args
+    url := "https://www.google.com/search?sca_esv=d220b328ad44e505&sxsrf=ACQVn0-N_PSr68nLqelTse_qZtFFHdrn5w:1708180318217&q=random+image&tbm=isch&source=lnms&prmd=ivsnbmtz&sa=X&ved=2ahUKEwiTldHIy7KEAxXnCbkGHeOvAfAQ0pQJegQIFBAB&biw=960&bih=959&dpr=1"
+    links, h1Text, err := Crawl(url)
 
-	links, h1Text, errImg, err := crawl(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-    errImg = downloadImage(url)
-    if errImg != nil {
-        log.Fatal(errImg)
+    if err != nil {
+        log.Fatal(err)
     }
-    fmt.Printf("Image downloaded successfully.")
 
-	fmt.Println("Links:")
-	for _, link := range links {
-		fmt.Println(link)
-	}
+    fmt.Println("Links:")
+    for _, link := range links {
+        fmt.Println(link)
+    }
 
-	fmt.Println("\n<h1> tag Text:")
-	fmt.Println(h1Text)
+    fmt.Println("\n<h1> tag Text:")
+    fmt.Println(h1Text)
 }
 
-func crawl(url string) ([]string, string, error, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, "", nil, err
-	}
-	defer resp.Body.Close()
+func Crawl(url string) ([]string, string, error) {
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, "", err
+    }
+    defer resp.Body.Close()
 
-	links, h1Text, errImg := extractElements(resp.Body, "a", "h1", "img")
+    links, h1Text, err := extractElements(resp.Body)
 
-	return links, h1Text, errImg, nil
+    return links, h1Text, nil
 }
 
 func extractElements(body io.Reader, tags ...string) ([]string, string, error) {
-	tokenizer := html.NewTokenizer(body)
-	links := []string{}
-	var h1Text string
-	var errImg error 
+    tokenizer := html.NewTokenizer(body)
+    links := []string{}
+    var h1Text string
 
-	for {
-		tokenType := tokenizer.Next()
-		switch tokenType {
-		case html.ErrorToken:
-			// End of the document
-			return links, h1Text, errImg 
-		case html.StartTagToken, html.EndTagToken:
-			token := tokenizer.Token()
-			for _, tag := range tags {
-				if token.Data == tag {
-					// Grab the link inside of <a href="example.com">
-					// TODO: make it so you grab the whole page inside instead of just the link.
-					if token.Data == "a" {
-						for _, attr := range token.Attr {
-							if attr.Key == "href" {
-								link := strings.TrimSpace(attr.Val)
-								links = append(links, link)
-							}
-						}
-						// Grab the text inside of <h1> tag
-					} else if tag == "h1" {
-						if tag == "h1" && tokenType == html.StartTagToken {
-							tokenType = tokenizer.Next()
-							h1Text = strings.TrimSpace(tokenizer.Token().Data)
-						}
-						// Download image
-					} else if tag == "img" {
-						if tokenType == html.StartTagToken {
-							for _, attr := range token.Attr {
-								if attr.Key == "src" {
-									imageUrl := strings.TrimSpace(attr.Val)
-									errImg = downloadImage(imageUrl)
-									if errImg != nil {
-                                        log.Printf("Error downloading image from %s: %v\n", imageUrl, errImg)
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    for {
+        tokenType := tokenizer.Next()
+        switch tokenType {
+        case html.ErrorToken:
+            // End of the document
+            return links, h1Text, nil 
+        case html.StartTagToken, html.SelfClosingTagToken:
+            token := tokenizer.Token()
+            // Grab the link inside of <a href="example.com">
+            if token.Data == "a" {
+                if tokenType != html.SelfClosingTagToken {
+                    for _, attr := range token.Attr {
+                        if attr.Key == "href" {
+                            link := strings.TrimSpace(attr.Val)
+                            links = append(links, link)
+                        }
+                    }
+                }
+                // Grab the text inside of <h1> tag
+            } else if token.Data == "h1" && tokenType == html.StartTagToken {
+                tokenType = tokenizer.Next()
+                h1Text = strings.TrimSpace(tokenizer.Token().Data)
+                // Download image
+            } else if  token.Data == "img" {
+                for _, attr := range token.Attr {
+                    if attr.Key == "src" {
+                        imageURL := strings.TrimSpace(attr.Val)
+                        err := Download(imageURL)
+                        if err != nil {
+                            log.Printf("Error downloading image from %s: %v\n", imageURL, err)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-func downloadImage(url string) error {
+
+func Download(url string) error {
     resp, err := http.Get(url)
     if err != nil {
-        log.Println(err)
+        return err
     }
     defer resp.Body.Close()
 
@@ -122,16 +110,14 @@ func downloadImage(url string) error {
     filepath := filepath.Join(downloadsDir, filename)
 
     file, err := os.Create(filepath)
-
     if err != nil {
-        log.Println(err)
+        return err
     }
     defer file.Close()
 
     _, err = io.Copy(file, resp.Body)
-
     if err != nil {
-        log.Println(err)
+        return err
     }
 
     fmt.Printf("Image downloaded to the %s path.", filepath)
